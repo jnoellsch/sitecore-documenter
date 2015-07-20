6fafdf988c5b2fd7924924b2b9d1c0258d4de596
Jim Noellsch <jnoellsch@rightpoint.com>
2015-07-20 12:57:47 -0500

    Included mapping interface.

---
+++ Src/SitecoreDocumentor.Web/Mapping/IObjectMapper.cs
@@ -0,0 +1,13 @@
+﻿namespace SitecoreDocumentor.Web.Mapping
+{
+    /// <summary>
+    /// Maps one object to another.
+    /// </summary>
+    public interface IObjectMapper<in TSource, out TDestination>
+    {
+        /// <summary>
+        /// Maps a <typeparamref name="TSource"/> object to a new <typeparamref name="TDestination"/> object.
+        /// </summary>
+        TDestination Map(TSource source);
+    }
+}
