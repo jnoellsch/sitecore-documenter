4bd1f0cee7cba55dfa5167bd765b26daa5f73eed
Jim Noellsch <jnoellsch@rightpoint.com>
2016-02-29 08:41:30 -0600

    Bumped version to 1.1, included core button into package.

--- Src/Documenter/Properties/AssemblyInfo.cs
+++ Src/Documenter/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.SharedSource.Documenter")]
 [assembly: AssemblyDescription("Produces detailed information of custom Sitecore renderings and templates for inclusion in technical documentation.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jim Noellsch")]
 [assembly: AssemblyProduct("Sitecore Documenter")]
 [assembly: AssemblyCopyright("Copyright © 2015-2016")]
 [assembly: AssemblyTrademark("")]
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
